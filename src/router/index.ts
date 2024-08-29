d41e3de755b76b099ba5fa34318e7d9029f9b56d
Roman Przybylski <romprzy81@gmail.com>
2024-08-29 18:11:50 +0200

    history fix - hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -71,7 +71,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   // extendRoutes: setupLayouts,
 })
